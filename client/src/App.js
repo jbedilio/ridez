1e7ee01e474391d6d507c289570441a95cd5bca9
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-11-01 18:24:15 -0400

    adding packages

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img className="App-logo"/>
+          <h1 className="App-title">Ridez Bitches</h1>
+        </header>
+        <p className="App-intro">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bp = require('body-parser');
+const mongoose = require('mongoose');
+const request = require('request');
+
+//setting mongoose to leverage built in ES6 Promises
+mongoose.Promise = Promise;
+
+//setting the PORT
+var PORT = process.env.PORT || 3000;
+
+//grabbing an instance of express
+const app = express();
+
+//serve static content for the app from the "public" directory in the app directory
+// app.use(express.static('client/build'));
+
+//setting the instance of express to use body-parser
+app.use(bp.urlencoded({ extended: true }));
+app.use(bp.json());
+
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect('mongodb://localhost/ridezdb', { useMongoClient: true });
+};
+
+const db = mongoose.connection;
+
+db.on('error', error => {
+    console.log('Mongoose error: ', error);
+});
+
+db.once('open', () => {
+    console.log('Mongoose in the hizzy!');
+});
+
+const routes = require('./controllers/ridezController.js');
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log(`axios ==> API Server now listening on PORT ${PORT}!`);
+});
