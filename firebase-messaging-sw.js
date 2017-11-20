440326d4f41e732c83c71a6b74cf7b4b14ff8124
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-11-19 22:09:30 -0500

    cleaning up the code

--- firebase-messaging-sw.js
+++
@@ -1,3 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase.js');
